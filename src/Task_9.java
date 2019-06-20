556b8c87ed4ff3ad9626b4fba0e703d756694c07
bpohenoe3d <xkotrusx@gmail.com>
2019-06-20 22:37:37 +0300

    Task 9

--- src/Task_9.java
+++ src/Task_9.java
@@ -5,15 +5,11 @@ public class Task_9 {
     public static void main(String[] agrs)
     {
         int[] a = new int[2];
-        int i = 0;
         int k =2;
         Scanner in = new Scanner(System.in);
-        for (int element : a)
-        {
-            System.out.print("Введите " + (i + 1) + "-й элемент массива: ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print("Введите цифру  [" + i + "]:");
             a[i] = in.nextInt();
-            System.out.println(a[i] + "");
-            i++;
         }
         for (int element : a)
 
