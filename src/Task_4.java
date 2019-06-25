6d0d1f950908af56a18faaddbfe4e73bb05c9f25
bpohenoe3d <xkotrusx@gmail.com>
2019-06-25 20:53:13 +0300

    Task 4

--- src/Task_4.java
+++ src/Task_4.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Task_4 {
     public static void  main (String[] args ){
         Scanner sn = new Scanner(System.in);
-        System.out.println("Введите число в шестнадцатиричной системе счисления:");
+        System.out.println("Введите число в бинарной системе счисления:");
         String bin = sn.nextLine();
-        int binNum = Integer.parseInt(bin, 16);
+        int binNum = Integer.parseInt(bin, 2);
         System.out.println(binNum);
     }
 }
