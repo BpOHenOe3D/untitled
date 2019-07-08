0d2dc15fd54330e804c0e3a567bae0ac49cc3f27
bpohenoe3d <xkotrusx@gmail.com>
2019-07-08 22:45:31 +0300

    Task 17

--- src/Task_14.java
+++ src/Task_14.java
@@ -1,11 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Task_14 {
-    public static void main(String args[]) {
-        Scanner scan = new Scanner(System.in);
+    public static void main(String args[]) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Введите число");
-        String s = scan.nextLine();
-        int x = Integer.parseInt(s);
+        String s = reader.readLine();
+        int x = Double.valueOf(s).intValue();
         float y =x;
         System.out.printf("%s\n%d\n%f\n",s,x,y);
     }}
