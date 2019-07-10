8dd6255f7cf6967ecfee50f7bd4fd6f047025c57
bpohenoe3d <xkotrusx@gmail.com>
2019-07-10 23:27:18 +0300

    Itog_1

---
+++ src/Itog_1.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Itog_1 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите число");
+        String str = reader.readLine();
+        int sum = 0, a;
+        int c;
+
+        for (int i = 0; i < str.length(); i++) {
+            c = str.charAt(str.length() - 1 - i);
+
+            if(c == '1' || c == '0') {
+                if (c == '1')
+                    a = 1;
+                else a = 0;
+            } else {
+                System.out.println("Неверный ввод!");
+                break;
+            }
+            sum += (a * Math.pow(2, i));
+        }
+
+        System.out.println("\"" + str + "\" -> " + sum);
+    }}
